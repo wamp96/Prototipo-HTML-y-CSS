089e2ab28cf2c20acff39c286a72eb73284a36c4
William Moreno <wamoreno57@gmail.com>
2023-11-30 18:21:14 -0500

    Actuaizacion front-end con boostrap

--- Proyecto/HTML, CSS ,PHP,MYSQL V2/Administrador/adm_crear_I.php
+++ Proyecto/HTML, CSS ,PHP,MYSQL V2/Administrador/adm_crear_I.php
@@ -3,42 +3,69 @@
         <div class="contenido d-flex bg-secondary">
             <div class="contenido1 col-sm-6">
                 <form action="adm_crear_C.php" method="post">
-                    <div class="col-form-label d-flex justify-content-evenly">
-                        <label>DOCUMENTO IDENTIDAD :</label>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label># IDENTIDAD :</label>
                         <input type="text" name="identidad">
                     </div>
-                    <div>
+                    <div class="col-form-label d-flex justify-content-between">
                         <label>NOMBRE :</label>  
                         <input type="text" name="nombre">
-                    </div>    
-                    <label>APELLIDO PARTENO: <input class="col-form-label" type="text" name="apellidop"></label>
-                    <label>APELLIDO MATERNO: <input class="col-form-label" type="text" name="apellidom"></label>
-                    <label>CORREO: <input type="text" class="col-form-label" name="correo"></label>
-                    <label>TELEFONO: <input type="text" class="col-form-label" name="telefono"></label>
+                    </div> 
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>APELLIDO PARTENO :</label>  
+                        <input type="text" name="apellidop">
+                    </div>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>APELLIDO MATERNO :</label>  
+                        <input type="text" name="apellidom">
+                    </div>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>CORREO :</label>  
+                        <input type="text" name="correo">
+                    </div>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>TELEFONO :</label>  
+                        <input type="text" name="telefono">
+                    </div>
                 </form>
             </div>
             <div class="contenido2 col-sm-6">
                 <form action="adm_crear_C.php" method="post">
-                    <label>USUARIO:<input type="text" class="col-form-label" name="usuario"></label>
-                    <label>ROL:<select name="rol" class="col-form-label">
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>USUARIO :</label>
+                        <input type="text" name="usuario">
+                    </div>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>ROL:</label> 
+                        <select name="rol">
                             <option value="1">TECNICO</option>
                             <option value="2">ANALISTA INVENTARIO</option>
                             <option value="3">USUARIO</option>
-                        </select></label>                
-                    <label>AREA:<select name="area" class="col-form-label">
+                        </select>
+                    </div>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>AREA:</label>
+                        <select name="area">
                             <option value="1">AUDITORIA</option>
-                            <option value="2">CONTABILIDAD</option>
+                            <option value="2">CONTABILIDAD       </option>
                             <option value="3">OFICINA</option>
                             <option value="4">DESARROLLO</option>
                             <option value="5">TECNOLOGIA</option>
-                        </select></label>
-                    <label>CIUDAD:<select name="ciudad" class="col-form-label">
+                        </select>
+                    </div>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>CIUDAD:</label>
+                        <select name="ciudad" class="col-form-label">
                             <option value="1">BOGOTA</option>
                             <option value="2">MEDELLIN</option>
                             <option value="3">CALI</option>
                             <option value="4">BARRANQUILLA</option>
-                        </select></label>
-                    <label>CONTRASEÑA: <span>*</span> <input class="col-form-label" type="password" name="contrasena"></label>
+                        </select>
+                    </div>
+                    <div class="col-form-label d-flex justify-content-between">
+                        <label>CONTRASEÑA: * </label>
+                        <input type="password" name="contrasena">
+                    </div>
                     <input type="submit" name="registrar" value="Registrar">
                 </form>
             </div>
