ae92c83e1d34283f833964199d81418478109296
wamp96 <wamoreno57@gmail.com>
2024-04-01 21:09:33 -0500

    Nuevo version del Proyecto Final

---
+++ Proyecto/public/assets/js/main.js
@@ -0,0 +1,34 @@
+class Usuario {
+    constructor() {
+
+    }
+
+}
+
+class Administrador extends Usuario {
+    constructor() {
+        super();
+    }
+
+}
+
+class AnalistaInventario extends Usuario {
+    constructor() {
+        super();
+    }
+
+}
+
+class Tecnico extends Usuario {
+    constructor() {
+        super();
+    }
+
+}
+
+class UsuarioFinal extends Usuario {
+    constructor() {
+        super();
+    }
+
+}
