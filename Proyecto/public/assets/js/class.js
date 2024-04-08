af3e4e2f83765beb508a1031cd8489a9c28c126d
wamp96 <wamoreno57@gmail.com>
2024-04-07 22:15:25 -0500

    Conexion de modulo Gestion Usuarios y Gestion Elementos con base de datos Firebase

--- Proyecto/public/assets/js/class.js
+++ Proyecto/public/assets/js/class.js
@@ -8,17 +8,17 @@
 
 class User {
   /*Metodo constructor*/
-  constructor(idcont,formUser) {
+  constructor(idcont,listUser) {
     this.objTbody = document.getElementById(idcont);
-    this.objformUser = document.getElementById(formUser);
+    this.objformUser = document.getElementById(listUser);
     this.URL = "https://wm-inventory-default-rtdb.firebaseio.com/api/usuario"
 
   }
 
   /*Método para obtener datos de los usuarios*/
+  
   async getDataUsers() {
-    console.log(this.objformUser)
-    return fetch(this.URL + ".json")
+    return fetch(this.URL + ".json")    
       .then((res) => {
         if (!res.ok) {
           console.log('Result: Problem');
@@ -60,18 +60,15 @@ class User {
     let contRow = 1;
     let rowTable = "";
     let btnActions = "";
-    let tableR = '<div style=" margin-top: 120px;" class="table-responsive tableApp"><table class="table table-dark table-striped table-hover"><thead><tr class="text-center"><th>#</th><th>NOMBRE</th><th>AREA</th><th>CIUDAD</th><th>CORREO</th><th>ID_PERFIL</th><th>NOMBRE_PERFIL</th><th>ESTADO</th><th>ROL</th><th>CONTRASEÑA</th><th>ACTIONS</th></tr></thead><tbody id="tbody">'
-    let tableA = ' </tbody><tfoot><tr class="text-center"><th>#</th><th>NOMBRE</th><th>AREA</th><th>CIUDAD</th><th>CORREO</th><th>ID_PERFIL</th><th>NOMBRE_PERFIL</th><th>ESTADO</th><th>ROL</th><th>CONTRASEÑA</th><th>ACTIONS</th></tr></tfoot></table></div>'
+
+    let tableR = '<div style=" margin-top: 120px;" id="tableUser" class="table-responsive tableApp"><div class="d-flex btn-add"><button type="button" onclick="createUser()" class="btn btn-success"><img class="img img-fluid"src="./assets/img/icons/plus-square-fill.svg">NUEVO</button></div><br><br><table class="table table-dark table-striped table-hover"><thead><tr class="text-center"><th>#</th><th>NOMBRE</th><th>N° DOCUMENTO</th><th>AREA</th><th>CIUDAD</th><th>CORREO</th><th>NOMBRE_PERFIL</th><th>ESTADO</th><th>ROL</th><th>TELEFONO</th><th>ACTIONS</th></tr></thead><tbody id="tbody">'
+    let tableA = ' </tbody><tfoot><tr class="text-center"><th>#</th><th>NOMBRE</th><th>N° DOCUMENTO</th><th>AREA</th><th>CIUDAD</th><th>CORREO</th><th>NOMBRE_PERFIL</th><th>ESTADO</th><th>ROL</th><th>TELEFONO</th><th>ACTIONS</th></tr></tfoot></table></div>'
     for (const user  in data) {
       
-      let datauser= data[user].perfil;
-      //console.log(datauser);
-      
-      let dataperfil = Object.values(datauser);
-
-      console.log(dataperfil);
-
+      /*let datauser= data[user].perfil;
+      let dataperfil = Object.values(datauser);*/
       let getId = "'" + user + "'";
+
       btnActions = '<div class="btn-group " role="group" aria-label="Basic mixed styles example">' +
         '<button type="button" onclick="showUser(' + getId + ')" class="btn btn-info"><img class="img img-fluid" src="./public/assets/img/icons/eye-fill.svg"></button>' +
         '<button type="button" onclick="editUser(' + getId + ')" class="btn btn-warning"><img class="img img-fluid" src="./public/assets/img/icons/pencil-square.svg"></button>' +
@@ -79,30 +76,25 @@ class User {
         '</div>';
         
       rowTable += "<tr>" +
-        "<td>" + contRow + "</td>" +
+        "<td>" + contRow + "</td>" +        
         "<td>" + data[user].nombre + "</td>" +
+        "<td>" + data[user].documento + "</td>" +
         "<td>" + data[user].area + "</td>" +
         "<td>" + data[user].ciudad + "</td>" +
         "<td>" + data[user].correo + "</td>" +
-        "<td>" + dataperfil[2]+ "</td>" +
-        "<td>" + dataperfil[3]+ "</td>" +
-        "<td>" + dataperfil[1]+ "</td>" +
-        "<td>" + dataperfil[4]+ "</td>" +
-        "<td>" + dataperfil[0]+ "</td>" +
+        "<td>" + data[user].perfil + "</td>" +
+        "<td>" + data[user].estado + "</td>" +
+        "<td>" + data[user].rol + "</td>" +
+        "<td>" + data[user].telefono + "</td>" +
+        /*"<td>" + dataperfil[3]+ "</td>" +*/
         "<td class='text-center'>" + btnActions + "</td>" +
         "<tr>";
       contRow++;
     }
     this.objTbody.innerHTML = tableR+rowTable+tableA;
   }
-
-  
-
-
-
-
-
   /*Método para crear el nuevo usuario de datos en formato Json de usuario.*/
+
   async setCreateUser(data) {
     return fetch(this.URL + ".json", {
       method: 'POST',
@@ -128,6 +120,7 @@ class User {
   }
 
   /*método para actualizar el usuario enviando un conjunto de datos en formato Json por parte del usuario.*/
+
   async setUpdateUser(id, data) {
     return fetch(this.URL + "/" + id + ".json", {
       method: 'PUT',
@@ -153,6 +146,7 @@ class User {
   }
 
   /*Método para eliminar el usuario. */
+
   async setDeleteUser(id) {
     return fetch(this.URL + "/" + id + ".json", {
       method: 'DELETE'
@@ -175,3 +169,158 @@ class User {
 }
 
 
+class elements {
+
+  constructor(idContAdm,listElement,listInv){
+    this.objcontAdm = document.getElementById(idContAdm);
+    this.objlistElement = document.getElementById(listElement);
+    this.objlistInv = document.getElementById(listInv);
+    this.URL = "https://wm-inventory-default-rtdb.firebaseio.com/api/elemento"
+  }  
+
+    /*Método para obtener datos de los elementos*/
+    async getDataElements() {
+      try {
+        const res = await fetch(this.URL + ".json");
+        if (!res.ok) {
+          console.log('Result: Problem');
+          return;
+        }
+        const data = await res.json();
+        this.setTableElements(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async getDataElement(id) {
+      return fetch(this.URL + "/" + id + ".json")
+        .then((res) => {
+          if (!res.ok) {
+            console.log('Result: Problem');
+            return;
+          }
+          return res.json();
+        })
+        .then((data) => {
+         
+          return data;
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally();
+    }
+
+    
+  /*Método para crear las filas de la tabla usando el formato Json del usuario.*/
+    setTableElements(data) {
+    let contRow = 1;
+    let rowTable = "";
+    let btnActions = "";
+    let tableR = '<div style=" margin-top: 120px;" id="createElement" class="table-responsive tableApp"><div class="d-flex btn-add"><button type="button" onclick="createElement class="btn btn-success"><img class="img img-fluid"src="./assets/img/icons/plus-square-fill.svg">NUEVO</button></div><br><br><table class="table table-dark table-striped table-hover"><thead><tr class="text-center"><th>#</th><th>NOMBRE</th><th>CATEGORIA</th><th>ESTADO</th><th>FECHA INGRESO</th><th>SERIAL</th><th>MARCA</th><th>MODELO</th><th>IMAGEN</th><th>TAMAÑA DISCO</th><th>TAMAÑO MEMORIA</th><th>VALOR U</th><th>ACTIONS</th></tr></thead><tbody id="tbody">'
+    let tableA = ' </tbody><tfoot><tr class="text-center"><th>#</th><th>NOMBRE</th><th>CATEGORIA</th><th>ESTADO</th><th>FECHA INGRESO</th><th>SERIAL</th><th>MARCA</th><th>MODELO</th><th>IMAGEN</th><th>TAMAÑA DISCO</th><th>TAMAÑO MEMORIA</th><th>VALOR U</th><th>ACTIONS</th></tr></tfoot></table></div>'
+    for (const element  in data) {
+      let getId = "'" + element + "'";
+   
+      btnActions = '<div class="btn-group " role="group" aria-label="Basic mixed styles example">' +
+        '<button type="button" onclick="showElement(' + getId + ')" class="btn btn-info"><img class="img img-fluid" src="./public/assets/img/icons/eye-fill.svg"></button>' +
+        '<button type="button" onclick="editElement(' + getId + ')" class="btn btn-warning"><img class="img img-fluid" src="./public/assets/img/icons/pencil-square.svg"></button>' +
+        '<button type="button" onclick="deleteElement(' + getId + ')" class="btn btn-danger"><img class="img img-fluid" src="./public/assets/img/icons/trash3-fill.svg"></button>' +
+        '</div>';
+        
+      rowTable += "<tr>" +
+        "<td>" + contRow + "</td>" +        
+        "<td>" + data[element].nombre + "</td>" +
+        "<td>" + data[element].categoria + "</td>" +
+        "<td>" + data[element].estado + "</td>" +
+        "<td>" + data[element].fecha_ingreso + "</td>" +
+        "<td>" + data[element].serial + "</td>" +
+        "<td>" + data[element].marca + "</td>" +
+        "<td>" + data[element].modelo + "</td>" +
+        "<td>" + data[element].imagen + "</td>" +
+        "<td>" + data[element].tamano_disco_duro + "</td>" +
+        "<td>" + data[element].tamano_memoria_ram + "</td>" +
+        "<td>" + data[element].valor_unitario + "</td>" +
+        /*"<td>" + dataperfil[3]+ "</td>" +*/
+        "<td class='text-center'>" + btnActions + "</td>" +
+        "<tr>";
+     
+      contRow++;
+    }
+    this.objcontAdm.innerHTML = tableR+rowTable+tableA;
+  }
+
+  async setCreateElement(data) {
+    return fetch(this.URL + ".json", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log('Result: Problem');
+          return;
+        }
+        return res.json();
+      })
+      .then((data) => {
+        this.getDataElements();
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally();
+  }
+
+  /*método para actualizar el usuario enviando un conjunto de datos en formato Json por parte del usuario.*/
+
+  async setUpdateElement(id, data) {
+    return fetch(this.URL + "/" + id + ".json", {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log('Result: Problem');
+          return;
+        }
+        return res.json();
+      })
+      .then((data) => {
+        this.getDataElements();
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally();
+  }
+
+  /*Método para eliminar el usuario. */
+
+  async setDeleteElement(id) {
+    return fetch(this.URL + "/" + id + ".json", {
+      method: 'DELETE'
+    })
+      .then((res) => {
+        if (!res.ok) {
+          console.log('Result: Problem');
+          return;
+        }
+        return res.json();
+      })
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally();
+  }
+  
+}
--- Proyecto/public/assets/js/main.js
+++ Proyecto/public/assets/js/main.js
@@ -1,28 +1,47 @@
 /**DEFINE*/
-const idcont = "container";
-const formUser = "gestionUsuario";
-const containerBase = new User(idcont,formUser);
-const idgestionUser = document.getElementById('gestionUsuario');
-const btnSubmit = document.getElementById('btnSubmit');
+//Constante globales 
 const preload = document.getElementById('preload');
-//const myModal = new bootstrap.Modal(document.getElementById("modalApp"), {});
 const textConfirm = "Press a button to Delete!\nAccept or Cancel.";
 
+//Constantes pagina administrador
+const idcont = "container-adm";
+const listUser = "gestion-usuario";
+const containerBase = new User(idcont,listUser);
+const formUser = document.getElementById('formUser');
+const btnSubmit = document.getElementById('btnSubmit');
+const myModal = new bootstrap.Modal(document.getElementById("modalApp"), {});
+
+
+
+//Variables pagina administrador
 var getIdUser = "";
+
+//Constantes pagina Analista de inventario 
+const idContAdm = "container-analista";
+const listElem = "gestion-elementos";
+const listInv = "gestion-inventario";
+const contElement = new elements(idContAdm,listElem,listInv);
+const formElements = document.getElementById('formElements');
+const elementModal = new bootstrap.Modal(document.getElementById("modal-element"), {});
+
+
+
+//Variables globales 
 var validate = true;
 
 
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**Funcion para obtener datos de usuario*/
-function getDataUser() {
-    //showPreload();
-    containerBase.getDataUsers().then((result) => {
-        // hidePreload();
 
+function getDataUser() {
+    showPreload();
+    containerBase.getDataUsers().then((result) => {
+       hidePreload();
     });
 }
 
 /**Funcion para crear usuario*/
+
 function createUser() {
     validate = true;
     cleanForm();
@@ -32,14 +51,15 @@ function createUser() {
 }
 
 /**Funcion para visualizar usuarios*/
+
 function showUser(id) {
     cleanForm();
     disableForm();
-    showPreload();
+     showPreload();
     const dataUser = containerBase.getDataUser(id);
     dataUser.then((data) => {
         setDataForm(data);
-        hidePreload();
+       hidePreload();
     });
     btnSubmit.disabled = true;
     showModal();
@@ -53,6 +73,7 @@ function editUser(id) {
     showPreload();
     getIdUser = id;
     const dataUser = containerBase.getDataUser(id);
+    
     dataUser.then((data) => {
         setDataForm(data);
         hidePreload();
@@ -62,6 +83,7 @@ function editUser(id) {
 }
 
 /**Funcion para eliminar usuarios*/
+
 function deleteUser(id) {
     showPreload();
     if (confirm(textConfirm) == true) {
@@ -73,11 +95,12 @@ function deleteUser(id) {
 }
 
 /**Funcion para enviar data al modal*/
-/*formUser.addEventListener('submit', (e) => {
+
+formUser.addEventListener('submit', (e) => {
     e.preventDefault();
     let inputId = document.getElementById('id');
     if (inputId.value.length === 0) {
-        inputId.value = uuid.v1();
+        inputId.value = generarUUID();
     }
     let elements = formUser.querySelectorAll('input');
     var jsonArray = {};
@@ -89,58 +112,201 @@ function deleteUser(id) {
     } else {
         containerBase.setUpdateUser(getIdUser, jsonArray).then(hideModal());
     }
-});*/
+});
 
-/**Funcion para visualizar el Modal*/
+/*Funcion para visualizar el Modal Usuarios*/
 function showModal() {
     myModal.show();
 };
 
-/**Funcion para ocultar Modal*/
+/**Funcion para ocultar Modal Usuarios*/
+
 function hideModal() {
     myModal.hide();
 }
 
-/**Funcion para limpiar formulario*/
+/**Funcion para limpiar formulario del Modal Usuarios*/
+
 function cleanForm() {
     formUser.reset();
 };
 
-/**Funcion para Habilitar Formulario*/
+/**Funcion para Habilitar Formulario del Modal Usuarios*/
 function enableForm() {
+    let user = formUser.querySelectorAll("input");
+    for (let i = 0; i < user.length; i++) {
+        user[i].disabled = false;
+    }
+};
+/**Funcion para deshabilitar el Formulario del Modal Usuarios */
+
+function disableForm() {
+    let user = formUser.querySelectorAll("input");
+    for (let i = 0; i < user.length; i++) {
+        user[i].disabled = true;
+    }
+};
+/*Funcion para el envio de data a el formulario del Modal Usuarios*/
+
+function setDataForm(data) {
     let elements = formUser.querySelectorAll("input");
+    for (let i = 0; i < elements.length; i++) {
+      document.getElementById(elements[i].id).value = data[elements[i].id];
+    }
+  };
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//FUNCIONES ELEMENTOS
+///////////////////////////////////////////////////////////////////////////////////////////
+/*Funcion para obtener datos de usuario*/
+function getDataElement() {
+    showPreload();
+       contElement.getDataElements().then((result) => {
+       hidePreload();
+    });
+}
+
+function createElement() {
+    validate = true;
+    cleanFormE();
+    enableFormE();
+    btnSubmit.disabled = false;
+    showModalE();
+}
+
+/**Funcion para visualizar usuarios*/
+function showElement(id) {
+    cleanFormE();
+    disableFormE();
+     showPreloadE();
+    const dataElement = containerElem.getDataElement(id);
+    dataElement.then((data) => {
+       setDataFormE(data);
+       hidePreload();
+    });
+    btnSubmit.disabled = true;
+    showModal();
+}
+
+/**Funcion para editar usuario*/
+function editElement(id) {
+    validate = false;
+    cleanFormE();
+    enableFormE();
+    //showPreload();
+    getIdUser = id;
+    const dataUser = containerElem.getDataElement(id);
+    
+    dataElement.then((data) => {
+        setDataFormE(data);
+       // hidePreload();
+    });
+    btnSubmit.disabled = false;
+    showModalE();
+}
+/**Funcion para eliminar usuarios*/
+function deleteElement(id) {
+    showPreload();
+    if (confirm(textConfirm) == true) {
+        containerBase.setDeleteElement(id).then((data) => {
+            getDataElement();
+        });
+    }
+    hidePreload();
+}
+/**Funcion para enviar data al modal*/
+formElements.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let inputId = document.getElementById('id');
+    if (inputId.value.length === 0) {
+        inputId.value = generarUUID();
+    }
+    let elements = formElements.querySelectorAll('input');
+    var jsonArray = {};
+    for (const elem of elements) {
+        jsonArray[elem.id] = elem.value;
+    }
+    if (validate) {
+        containerElem.setCreateElement(jsonArray).then(hideModal());
+    } else {
+        containerElem.setUpdateElement(getIdUser, jsonArray).then(hideModal());
+    }
+});
+/**Funcion para visualizar el Modal Usuarios*/
+function showModalE() {
+    elementModal.show();
+};
+/**Funcion para ocultar Modal Usuarios*/
+function hideModalE() {
+    elementModal.hide();
+}
+/**Funcion para limpiar formulario del Modal Usuarios*/
+function cleanFormE() {
+    elementModal.reset();
+};
+/**Funcion para Habilitar Formulario del Modal Usuarios*/
+function enableFormE() {
+    let elements = formElements.querySelectorAll("input");
     for (let i = 0; i < elements.length; i++) {
         elements[i].disabled = false;
     }
 };
 
-/**Funcion para deshabilitar el Formulario*/
-function disableForm() {
-    let elements = formUser.querySelectorAll("input");
+function disableFormE() {
+    let elements = formElements.querySelectorAll("input");
     for (let i = 0; i < elements.length; i++) {
         elements[i].disabled = true;
     }
 };
-
-/**FFuncion para el envio de data a el fomulario*/
-function setDataForm(data) {
-    let elements = formUser.querySelectorAll("input");
+/*Funcion para el envio de data a el formulario del Modal Usuarios*/
+function setDataFormE(data) {
+    let elements = formElements.querySelectorAll("input");
     for (let i = 0; i < elements.length; i++) {
         document.getElementById(elements[i].id).value = data[elements[i].id];
     }
 };
-/*
-/**Funcion de precarga de pagina
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Funcion para generar un UUID*/
+function generarUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
+/*Funcion de precarga de pagina*/
 function showPreload() {
     preload.style.display = "block";
-};*/
+};
 
-/**Funcion precarga ocula
+/*Funcion precarga oculta*/
 function hidePreload() {
     preload.style.display = "none";
-}*/
+}
 
 /*Vista de carga de usuarios primer evento que se ejecuta al cargar la pagina**/
-/*window.addEventListener('load', (e) => {
-    getDataUser();
-});*/
+window.addEventListener('load', (e) => {
+    hidePreload()
+});
